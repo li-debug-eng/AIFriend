dc0eb72e0eeceb627afad30318c5993667fa172c
Jibo Li <li_6825@126.com>
2026-04-29 22:03:33 +0800

    资料更新后端api

---
+++ backend/web/views/utils/photo.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+import os
+
+def remove_old_photo(photo):
+    if (photo) and photo.name != 'user/photos/default.jpg':
+        old_path =  settings.MEDIA_ROOT / photo.name
+        if old_path.exists():
+            os.remove(old_path)
+
