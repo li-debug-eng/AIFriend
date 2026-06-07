b9212e6d9f58c17ae2f93919e66d13140cb586b9
Li Jibo <li_6825@126.com>
2026-06-07 16:51:20 +0800

    实现好友列表前端

--- frontend/src/components/character/Character.vue
+++ frontend/src/components/character/Character.vue
@@ -8,7 +8,8 @@ import api from "@/js/http/api.js";
 import ChatField from "@/components/character/chat_field/ChatField.vue";
 import {useRouter} from "vue-router";
 
-const props = defineProps(['character','canEdit'])
+
+const props = defineProps(['character','canEdit','canRemoveFriend','friendId'])
 const emit = defineEmits(['remove'])
 const isHover = ref(false)
 const user = useUserStore()
@@ -24,6 +25,19 @@ async function handleRemoveCharacter(){
     })
     if (res.data.result === 'success'){
       emit('remove',props.character.id)
+    }
+  }catch(err){
+
+  }
+}
+
+async function handleRemoveFriend(){
+  try{
+    const res = await api.post('api/friend/remove/',{
+      friend_id: props.friendId,
+    })
+    if (res.data.result === 'success'){
+      emit('remove',props.friendId)
     }
   }catch(err){
 
@@ -46,7 +60,7 @@ async function openChatField(){
         chatFieldRef.value.showModal()
       }
     }catch(err){
-      console.log(err)
+
     }
   }
 }
@@ -60,10 +74,15 @@ async function openChatField(){
         <img :src="character.background_image" alt="" />
         <div class="absolute left-0 top-50 w-60 h-50 bg-linear-to-t from-black/40 to-transparent"></div>
         <div v-if="canEdit&& character.author.user_id === user.id" class="absolute top-50 right-0">
-          <RouterLink :to="{name:'update-character',params:{character_id:character.id}}" class="btn btn-circle btn-ghost bg-transparent">
+          <RouterLink @click.stop :to="{name:'update-character',params:{character_id:character.id}}" class="btn btn-circle btn-ghost bg-transparent">
             <UpdateIcon/>
           </RouterLink>
-          <button @click="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
+          <button @click.stop="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
+            <RemoveIcon/>
+          </button>
+        </div>
+        <div v-if="canRemoveFriend" class="absolute right-0 top-50">
+          <button @click.stop="handleRemoveFriend" class="btn btn-circle btn-ghost bg-transparent">
             <RemoveIcon/>
           </button>
         </div>
--- frontend/src/views/friend/FriendIndex.vue
+++ frontend/src/views/friend/FriendIndex.vue
@@ -1,9 +1,98 @@
 <script setup>
+import {nextTick, onBeforeMount, onBeforeUnmount, onMounted, ref, useTemplateRef} from "vue";
+import api from "@/js/http/api.js";
+import Character from "@/components/character/Character.vue";
 
+const friends=ref([])
+const sentinelRef= useTemplateRef('sentinel-ref')
+const isLoading = ref(false);
+const hasFriend = ref(true);
+
+function checkSentinelRef(){
+  if(!sentinelRef.value) return false
+  const rect = sentinelRef.value.getBoundingClientRect()
+  return rect.top < window.innerHeight && rect.bottom >0
+}
+
+function removeFriend(friendId){
+  friends.value = friends.value.filter(f =>f.id !== friendId)
+}
+
+async function loadMore() {
+  if (isLoading.value || !hasFriend.value) return
+  isLoading.value = true
+  let newFriends = []
+  try{
+
+    const res = await api.get('/api/friend/get_list/',{
+      params: {
+        items_count:friends.value.length
+      }
+    })
+    const data =res.data
+    if(data.result ==='success'){
+      newFriends = data.friends
+    }
+  }catch(err){
+
+  }finally {
+    isLoading.value = false
+    if(newFriends.length === 0){
+      hasFriend.value = false
+    }else{
+      friends.value.push(...newFriends)
+      await nextTick()
+
+      if(checkSentinelRef){
+        await loadMore()
+      }
+    }
+  }
+}
+let observer = null
+
+onMounted(async() => {
+  await loadMore()
+  await nextTick()
+
+  observer = new IntersectionObserver(
+      entries =>{
+        entries.forEach(entry =>{
+          if(entry.isIntersecting){
+            loadMore()
+          }
+        })
+      },
+      {root:null,rootMargin:'2px',threshold:0}
+  )
+
+  observer.observe(sentinelRef.value)
+
+})
+
+onBeforeUnmount(() => {
+  observer?.disconnect()
+})
 </script>
 
 <template>
-好友
+  <div class="flex flex-col items-center mb-12">
+    <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+      <Character
+        v-for="friend in friends"
+        :key="friend.id"
+        :character="friend.character"
+        :canRemoveFriend="true"
+        :friendId="friend.id"
+        @remove="removeFriend"
+      />
+    </div>
+    <div ref="sentinel-ref" class=" h-2 mt-8"></div>
+    <div v-if="isLoading" class="text-gray-500 mt-4">加载中...</div>
+    <div v-if="!hasFriend" class="text-gray-500 mt-4">没有更多聊天了</div>
+  </div>
+
+
 </template>
 
 <style scoped>
